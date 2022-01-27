29a7d1d81d8621be0fae84b44b1cc390904b4e14
gost1k <gost1k337@gmail.com>
2022-01-27 22:19:15 +0700

    feat: add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const PORT = process.env.PORT || 5000;
+
+  const config = new DocumentBuilder()
+    .setTitle('Unistory task')
+    .setDescription('The unistory API description')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, document);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  await app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 }
+
 bootstrap();
